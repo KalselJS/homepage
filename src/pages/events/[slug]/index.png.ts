cbe3c7272b8733a1f56241d6dd1dbd2e121afdba
ridho <ulunmuhammadridho@gmail.com>
2024-06-24 16:56:50 +0800

    chore(config): change from post into event

--- src/pages/events/[slug]/index.png.ts
+++ src/pages/events/[slug]/index.png.ts
@@ -4,17 +4,20 @@ import { generateOgImageForPost } from "@utils/generateOgImages";
 import { slugifyStr } from "@utils/slugify";
 
 export async function getStaticPaths() {
-  const posts = await getCollection("blog").then(p =>
+  const events = await getCollection("event").then(p =>
     p.filter(({ data }) => !data.draft && !data.ogImage)
   );
 
-  return posts.map(post => ({
-    params: { slug: slugifyStr(post.data.title) },
-    props: post,
+  return events.map(event => ({
+    params: { slug: slugifyStr(event?.data.title) },
+    props: event,
   }));
 }
 
 export const GET: APIRoute = async ({ props }) =>
-  new Response(await generateOgImageForPost(props as CollectionEntry<"blog">), {
-    headers: { "Content-Type": "image/png" },
-  });
+  new Response(
+    await generateOgImageForPost(props as CollectionEntry<"event">),
+    {
+      headers: { "Content-Type": "image/png" },
+    }
+  );
